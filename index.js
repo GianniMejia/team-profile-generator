feadf6df6a23ae5bdf9d45ae11ba8e1413c3804f
GianniMejia <101169251+GianniMejia@users.noreply.github.com>
2022-07-26 20:37:30 -0700

    updated index.js with engineer, intern and manager

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 import inquirer from "inquirer";
 
 try {
-  const answers = await inquirer.prompt([
-    /* Pass your questions in here */
-    {
-      type: "input",
-      name: "Manager Name",
-      message: "What is your team manager's name?",
-    },
-    {
-      type: "input",
-      name: "Manager Employee ID",
-      message: "What is your team manager's employee ID?",
-    },
-    {
-      type: "input",
-      name: "Manager Email",
-      message: "What is your team manager's email address?",
-    },
-    {
-      type: "input",
-      name: "Manager Office Number",
-      message: "What is your team manager's office number?",
-    },
-  ]);
+  const roster = {
+    manager: null,
+    engineers: [],
+    interns: [],
+  };
+
+  roster.manager = await getManagerInfo();
 
   let menuAnswers;
   do {
@@ -38,40 +22,20 @@ try {
 
     switch (menuAnswers["Menu"]) {
       case "Enter an Engineer":
-        // Prompt for engineer
-        const answers = await inquirer.prompt([
-          {
-            type: "input",
-            name: "Engineer Name",
-            message: "What is the Engineer's name?",
-          },
-          {
-            type: "input",
-            name: "Engineer Employee ID",
-            message: "What is the Engineer's employee ID?",
-          },
-          {
-            type: "input",
-            name: "Engineer Email",
-            message: "What is the Engineer's email address?",
-          },
-          {
-            type: "input",
-            name: "Engineer Office Number",
-            message: "What is the Engineer's office number?",
-          },
-          {
-            type: "input",
-            name: "Engineer GitHub",
-            message: "What is the Engineer's GitHub username?",
-          },
-        ]);
+        {
+          // Prompt for engineer
+          roster.engineers = [...roster.engineers, await getEngineer()];
+        }
         break;
       case "Enter an Intern":
-        // Prompt for intern
+        {
+          // Prompt for intern
+          roster.interns = [...roster.interns, await getIntern()];
+        }
         break;
       case "Finish":
         // Generate the HTML file
+        console.log("roster: ", roster);
         break;
     }
   } while (menuAnswers["Menu"] != "Finish");
@@ -81,4 +45,80 @@ try {
   } else {
     // Something else went wrong
   }
+}
+
+async function getManagerInfo() {
+  return await inquirer.prompt([
+    /* Pass your questions in here */
+    {
+      type: "input",
+      name: "name",
+      message: "What is your team manager's name?",
+    },
+    {
+      type: "input",
+      name: "employeeId",
+      message: "What is your team manager's employee ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your team manager's email address?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is your team manager's office number?",
+    },
+  ]);
+}
+
+async function getEngineer() {
+  return await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the Engineer's name?",
+    },
+    {
+      type: "input",
+      name: "employeeId",
+      message: "What is the Engineer's employee ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the Engineer's email address?",
+    },
+    {
+      type: "input",
+      name: "githubUsername",
+      message: "What is the Engineer's GitHub username?",
+    },
+  ]);
+}
+
+async function getIntern() {
+  return await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the Intern's name?",
+    },
+    {
+      type: "input",
+      name: "employeeId",
+      message: "What is the Intern's employee ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the Intern's email address?",
+    },
+    {
+      type: "input",
+      name: "schoolName",
+      message: "What is the Intern's school name?",
+    },
+  ]);
 }
